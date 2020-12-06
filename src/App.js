77a6fbb01d7fe3a247aed74df8ea33c261c51ca4
vinandros <vinandros@gmail.com>
2020-12-05 23:29:33 -0600

    file structure and basic routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import SignUp from "./components/auth/SignUp";
+import Projects from "./components/projects/Projects";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/projects" component={Projects} />
+      </Switch>
+    </Router>
   );
 }
 
