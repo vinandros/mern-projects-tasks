a003d577d61791f5fdd340551a47cd425d0bbc1c
vinandros <vinandros@gmail.com>
2020-12-06 18:51:11 -0600

    Login and Signup view

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,7 +1,65 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Login = () => {
-  return <h1>login</h1>;
+  const [formData, setFormData] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const { email, password } = formData;
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              onChange={handleChange}
+              placeholder="Put your email here!"
+              value={email}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={handleChange}
+              placeholder="Put your password here!"
+              value={password}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              value="Sign In"
+              className="btn btn-primario btn-block"
+            />
+          </div>
+        </form>
+        <Link to={"/signup"} className="enlace-cuenta">
+          {" "}
+          Get an account here!
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,7 +1,89 @@
 import React from "react";
+import { Link } from "react-router-dom";
+const SingUp = () => {
+  const [formData, setFormData] = React.useState({
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+    userName: "",
+  });
 
-const SignUp = () => {
-  return <h1>nueva cuenta</h1>;
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const { email, password, userName, passwordConfirmation } = formData;
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Sign Up</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="campo-form">
+            <label htmlFor="userName">User Name</label>
+            <input
+              type="text"
+              name="userName"
+              id="userName"
+              onChange={handleChange}
+              placeholder="Put your user name here!"
+              value={userName}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              onChange={handleChange}
+              placeholder="Put your email here!"
+              value={email}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={handleChange}
+              placeholder="Put your password here!"
+              value={password}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="passwordConfirmation">Password Confirmation</label>
+            <input
+              type="password"
+              name="passwordConfirmation"
+              id="passwordConfirmation"
+              onChange={handleChange}
+              placeholder="Repit your password here!"
+              value={passwordConfirmation}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              value="Sign Up"
+              className="btn btn-primario btn-block"
+            />
+          </div>
+        </form>
+        <Link to={"/"} className="enlace-cuenta">
+          {" "}
+          Go Login!
+        </Link>
+      </div>
+    </div>
+  );
 };
 
-export default SignUp;
+export default SingUp;
